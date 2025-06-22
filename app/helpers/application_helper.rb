dc3094bdf3629b18b2f5bf9bd69bd684acacc4c2
Haoming Li <16haomingli@gmail.com>
2025-06-22 17:04:26 +0100

    updated frontend to be more consistent and clean

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    def status_badge_color(status)
+        case status
+        when 'active' then 'success'
+        when 'discharged' then 'warning'
+        when 'deceased' then 'danger'
+        else 'secondary'
+        end
+    end
 end
